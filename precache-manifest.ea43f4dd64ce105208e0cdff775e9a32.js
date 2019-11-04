6dff6bad9b8b9edd8f8515ecdec12daf2675e048
Dariusz Okonski <darek200180@gmail.com>
2019-11-04 12:49:08 +0000

    Updates

---
+++ precache-manifest.ea43f4dd64ce105208e0cdff775e9a32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8639d1a40d70564411fb2aca7ef45317",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f1870afbddd9db3aa06d",
+    "url": "/monsters-rolodex/static/css/main.1e7d3f89.chunk.css"
+  },
+  {
+    "revision": "f07cd14c01013401b67f",
+    "url": "/monsters-rolodex/static/js/2.3e423f20.chunk.js"
+  },
+  {
+    "revision": "f1870afbddd9db3aa06d",
+    "url": "/monsters-rolodex/static/js/main.3c422ad8.chunk.js"
+  },
+  {
+    "revision": "0eae8100edc717a21893",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa45f042.js"
+  }
+]);
