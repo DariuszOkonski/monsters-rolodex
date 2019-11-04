61a4300ef5d5653b62913d28e63beafe752d385b
Dariusz Okonski <darek200180@gmail.com>
2019-11-04 14:18:25 +0000

    add h1 to App component

--- src/App.js
+++ src/App.js
@@ -32,6 +32,8 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
+
         <SearchBox
           placeholder="Search monsters..."
           handleChange={this.handleChange}
